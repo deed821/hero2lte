d290fdfcfc914af240e4772ef510db23160db087
deedwar <deed821@gmail.com>
2017-12-31 06:14:46 +1100

    hero: modify for aicp
    Change-Id: Ied1abbb43dae7877ad89a0e7baa93a9af0bbcdcd

--- aicp.mk
+++ aicp.mk
@@ -17,28 +17,22 @@
 $(call inherit-product, device/samsung/hero2lte/device_hero2lte.mk)
 $(call inherit-product, device/samsung/hero-common/hero-common.mk)
 
-# Sample: This is where we'd set a backup provider if we had one
-# $(call inherit-product, device/sample/products/backup_overlay.mk)
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-# Get the prebuilt list of APNs
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
-
-PRODUCT_PACKAGE_OVERLAYS += vendor/omni/overlay/CarrierConfig
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := omni_hero2lte
+PRODUCT_NAME := aicp_hero2lte
 PRODUCT_DEVICE := hero2lte
 PRODUCT_MODEL := SM-G935F
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
 
-# must be before including omni part
-TARGET_BOOTANIMATION_SIZE := 1080p
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+-include vendor/aicp/configs/bootanimation.mk
