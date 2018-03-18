2489d8e411f57ca41bdfc103b221abf79d20b324
deed821 <deed821@gmail.com>
2018-03-18 18:10:34 +1100

    hero: modify for omnirom

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device_hero2lte.mk \
+    $(LOCAL_DIR)/omni_hero2lte.mk
--- omni_hero2lte.mk
+++ omni_hero2lte.mk
@@ -17,17 +17,25 @@
 $(call inherit-product, device/samsung/hero2lte/device_hero2lte.mk)
 $(call inherit-product, device/samsung/hero-common/hero-common.mk)
 
-# Inherit from the 64 bit configuration
+# $(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Get the prebuilt list of APNs
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# must be before including omni part
+TARGET_BOOTANIMATION_SIZE := 1080p
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+ALLOW_MISSING_DEPENDENCIES=true
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_hero2lte
+PRODUCT_NAME := omni_hero2lte
 PRODUCT_DEVICE := hero2lte
 PRODUCT_MODEL := SM-G935F
 PRODUCT_BRAND := samsung
