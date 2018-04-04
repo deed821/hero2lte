e0909dbf64689986eec6dabfa05d116bdb65381e
deed821 <deed821@gmail.com>
2018-04-05 04:49:41 +1000

    hero: change config from lineageos to rr
    Change-Id: If7668b82b02d5c5863b3d2dfbd46cf29e63a7ad9

--- rr.mk
+++ rr.mk
@@ -17,17 +17,15 @@
 $(call inherit-product, device/samsung/hero2lte/device_hero2lte.mk)
 $(call inherit-product, device/samsung/hero-common/hero-common.mk)
 
-# Inherit from the 64 bit configuration
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common ResurrectionRemix stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_hero2lte
+PRODUCT_NAME := rr_hero2lte
 PRODUCT_DEVICE := hero2lte
 PRODUCT_MODEL := SM-G935F
 PRODUCT_BRAND := samsung
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,1 @@
-add_lunch_combo lineage_hero2lte-eng
-add_lunch_combo lineage_hero2lte-userdebug
-add_lunch_combo lineage_hero2lte-user
+add_lunch_combo rr_hero2lte-userdebug
